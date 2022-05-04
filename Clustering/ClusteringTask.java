6baf01b885883278c7b931bf55ffbb84779995e6
Zachary G <54991604+za08gilm@users.noreply.github.com>
2022-05-04 16:50:32 -0400

    Add files via upload

---
+++ Clustering/ClusteringTask.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Random;
+
+import weka.core.Instances;
+import weka.clusterers.ClusterEvaluation;
+import weka.clusterers.EM;
+
+/**
+ * Write a description of class ClusteringTask here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ClusteringTask
+{
+    public static void main(String args[]) throws Exception{
+
+        //load data
+        Instances data = new Instances(new BufferedReader(new FileReader("data/bank-data.arff")));
+
+        // new instance of clusterer
+        EM model = new EM();
+        // build the clusterer
+        model.buildClusterer(data);
+        System.out.println(model);
+
+        double logLikelihood = ClusterEvaluation.crossValidateModel(model, data, 2, new Random(1));
+        System.out.println(logLikelihood);
+        System.exit(1);
+    }
+}
